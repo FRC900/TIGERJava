815dd4764882eebc37f796ce8657378848bb5a36
o-fugi <o.fugikawa@gmail.com>
2018-06-01 13:09:47 -0400

    Added a ridiculous amount of comments

--- src/org/usfirst/frc/team900/robot/Autonomous.java
+++ src/org/usfirst/frc/team900/robot/Autonomous.java
@@ -2,22 +2,26 @@ package org.usfirst.frc.team900.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
 
+//the autonomous action is just a way to package all autonomous commands into single lines
 public class Autonomous {
-	private Tankdrive driveBase;
+	public Tankdrive driveBase;
 	
-	public Autonomous(Tankdrive drive) {
-		driveBase = drive;
+	//constructor -- add all subsystems that you want to move during auto
+	public Autonomous(Tankdrive driveBase_) {
+		driveBase = driveBase_;
 	}
 	
 	public void init() {
 	}
 	
+	//runs periodically throughout autonomous
 	public void run() {
-		if(DriverStation.getInstance().getMatchTime() > (15 - Constants.autoTime)) {
-			driveBase.drive(Constants.autoSpeed, Constants.autoSpeed); //from -1 to 1
+		if(DriverStation.getInstance().getMatchTime() > (15 - Constants.autoTime)) { //limit the amount of time that the robot drives
+			driveBase.drive(Constants.autoSpeed, Constants.autoSpeed); //speed input is from -1 to 1
 		}
 	}
 	
+	//make sure that the robot stops driving at the end of autonomous
 	public void end() {
 		driveBase.drive(0.0, 0.0);
 	}
--- src/org/usfirst/frc/team900/robot/Robot.java
+++ src/org/usfirst/frc/team900/robot/Robot.java
@@ -1,120 +1,67 @@
-package org.usfirst.frc.team900.robot;
+//this defines where the file is
+package org.usfirst.frc.team900.robot; 
 
-import org.usfirst.frc.team900.*;
-
-import java.util.List;
-
+//all of these import libraries that are used in this file 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
-
-//import org.usfirst.frc.team900.robot.commands.*;
-
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
+//now we create a class called Robot, which is a subclass of IterativeRobot. IterativeRobot is a FRC-defined thing.
 public class Robot extends IterativeRobot {
-	public static Tankdrive driveBase;
-	public static XboxController controller;
+	public static Tankdrive driveBase; //initialize the drive base
+	public static XboxController controller; //initialize the joystick
 	
-	public static Autonomous autoAction;
-	public static DriverStation ds;
-	/*public static Robot robot;
-	public static Joystick joy1, joy2;
-	public static Swerve swerve;
-	public static Gyro gyro;
-	public static Shooter shooter;
-	//	public static DOF9 fancygyro;
-	//	static ADC adc;
+	public static Autonomous autoAction; //initialize the auto action
+	public static DriverStation ds; //initialize the driver station
 
-	Command autoCommand;
-	SendableChooser<Command> autoChooser;
-	double test;*/
-
+	//this function is run when the robot is initialized
 	public void robotInit() {
-		driveBase = new Tankdrive();
-		driveBase.setup();
+		driveBase = new Tankdrive(); //create the drive base (defines all of the motor controllers and related terms)
+		driveBase.setup(); //setup followers, reverse the outputs of certain Talons, etc.
 		
 		controller = new XboxController(0);
 		autoAction = new Autonomous(driveBase);
-		ds = DriverStation.getInstance();
+		ds = DriverStation.getInstance(); /* this is how you access information from the Driver Station. See more at 
+										   * https://wpilib.screenstepslive.com/s/currentCS/m/java/l/599722-driver-station-input-overview
+										   */
 		
+		//these two lines display information about the autonomous action on the SmartDashboard
 		SmartDashboard.putNumber("Drive Forward Speed", Constants.autoSpeed);
 		SmartDashboard.putNumber("Drive Forward Time", Constants.autoTime);
-	    /*SmartDashboard.putNumber("Speed", .5);
-	    SmartDashboard.putNumber("x", -15);
-	    SmartDashboard.putNumber("y", 75);
-	    SmartDashboard.putNumber("driveSpeed", .5);
-        SmartDashboard.putNumber("angle", 60);
-        SmartDashboard.putNumber("duration", 3);
-		test = 0;
-		robot = this;
-		System.out.println("robot init");
-		joy1 = new Joystick(0);
-		joy2 = new Joystick(1);
-		gyro = new Gyro();
-		//		fancygyro = new DOF9(edu.wpi.first.wpilibj.I2C.Port.kOnboard,0,-1);
-		swerve = new Swerve(joy1, gyro);
-		winch = new Winch(joy2);
-		gear = new GearIntake(joy2);
-		shooter = new Shooter(joy2);
-		autoChooser = new SendableChooser<Command>();
-		autoChooser.addObject("Middle Gear", new MiddleGearAuto());
-		autoChooser.addDefault("fortyKPAuto",  new fortyKPAuto());
-		autoChooser.addObject("Boiler Gear", new BoilerSideAuto());
-		autoChooser.addObject("Gear Reset", new GearReset());
-		autoChooser.addObject("SideGearAutoRight", new SideGearAutoRight());
-		//autoChooser.addObject("Shooty", new Shoot(15));
-		//		adc=new ADC(edu.wpi.first.wpilibj.I2C.Port.kMXP);
-		SmartDashboard.putData("Autonomous mode chooser", autoChooser);*/
 	}
 
-
+	//function is run when autonomous begins
 	public void autonomousInit() {
 		autoAction.init();
 	}
-
+	
+	//function is called over and over again during autonomous
 	public void autonomousPeriodic() {
 		autoAction.run();
-		Scheduler.getInstance().run();
+		Scheduler.getInstance().run(); //what does this actually do? no one knows...
 	}
-
-	/**
-	 * This function is called periodically during operator control
-	 */
+	
+	//this function is called when autonomous ends
 	public void teleopInit(){
-		autoAction.end();
+		autoAction.end(); //make the robot stop performing auto actions!
 	}
+	
+	//this function is called over and over again during autonomous
 	public void teleopPeriodic() {
-		driveBase.drive(controller.getY(GenericHID.Hand.kLeft), controller.getY(GenericHID.Hand.kRight));
-		/*swerve.move();
-		shooter.shoot();
-		gear.gear();
-		winch.winch();
-		SmartDashboard.putNumber("Gyro", gyro.getAngle());*/
+		driveBase.drive(controller.getY(GenericHID.Hand.kLeft), controller.getY(GenericHID.Hand.kRight)); //tell the robot to drive with inputs from the controllers
 	}
+	
+	//these three are probably not useful. ignore for now.
 	public void disabledInit() {
-		//if (autoCommand != null) autoCommand.cancel();
 	}
+	
 	public void disabledPeriodic(){
-		//swerve.smartDash();
 	}
-	/**
-	 * This function is called periodically during test mode
-	 */
+	
 	public void testPeriodic() {
-
 	}
 
 }
